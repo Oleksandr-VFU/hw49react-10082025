c3ac13ae49638474b68b2a7c36a7ab084ff53063
Oleksandr Komnatskyi <akomnatskyi@gmail.com>
2025-08-10 11:18:35 +0300

    Chore: configure Vitest testing environment

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import vitest from 'eslint-plugin-vitest'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,15 +16,31 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...vitest.environments.env.globals // Додано глобальнізмінні Vitest (describe, test, expect тощо)
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {
+      React,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      vitest // Додано плагін Vitest для підтримки тестів
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...React.configs.recommended.rules,
+      ...React.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...vitest.configs.recommended.rules, // Додано рекомендовані правила для Vitest
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true }]
     },
   },
 ])
